06ba700dfff49fb556d8d2a70617fbc4d15c8f8d
fedlucchetti <fedlucchetti@gmail.com>
2025-03-12 10:20:02 +0100

    correct setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 from setuptools import setup, find_packages
 
@@ -6,11 +8,17 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
+    author="Federico Lucchetti",
+    author_email='federico.lucchetti@unil.ch',
+    python_requires='>=3.6',
     name = "mrsitoolbox",
     version = "1.0.0",
     description = ("Analysis toolbox for MRSI data"),
-    license = "BSD",
-    keywords = "example documentation tutorial",
+    license = "License :: Other/Proprietary License",
+    include_package_data=True,
+    keywords='mrsitoolbox',
+    url='https://github.com/MRSI-Psychosis-UP/Metabolic-Connectome.git',
+    name='mrsitoolbox',
     packages=find_packages(include=['tools','graphplot','filters',
                                     'registration','connectomics',
                                     'bids']),
@@ -20,3 +28,6 @@ setup(
         "License :: Other/Proprietary License"
     ],
 )
+
+
+
